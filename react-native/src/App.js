03ddc0f2d4ada7c8e1ecc52693926faee337ff02
RNCST <rk51320928@gmail.com>
2021-09-08 11:10:57 +0900

    21/09/08 react-native ing

--- react-native/src/App.js
+++ react-native/src/App.js
@@ -1,27 +1,53 @@
-import logo from './logo.svg';
-import { Component } from 'react';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
-class App extends Component{
+class App extends Component {
   render() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <div className="App">
+      <Subject title="WEB" Text="WebText"></Subject>
+      <Subject title="REACT" Text="REACTTtext"></Subject>
+      <TOC list1Titie="list1" list2Titie="list2" list3Titie="list3"></TOC>
+      <Contents title="Contenst title" desc="description"></Contents>
+    
+      </div>;
+  }
+}
+
+class Subject extends Component{
+  render() {
+      return (
+        <header>
+          <h1>{this.props.title}</h1>
+          Header text {this.props.Text}
+        </header>
+      );
+  }
+}
+
+class TOC extends Component{
+  render() {
+    return (
+      <nav>
+        <h3>
+        <li>1.{this.props.list1Titie}</li>
+        <li>2.{this.props.list2Titie}</li>
+        <li>3.{this.props.list3Titie}</li>
+        </h3>
+      </nav>
+    )
+  }
+}
+
+class Contents extends Component{
+  render(){
+    return (
+      <article>
+      <h3>
+        {this.props.title}
+      </h3>
+      {this.props.desc}
+      </article>
+    )
   }
 }
 
